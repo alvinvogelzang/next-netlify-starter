41b8ee42b8c170174650f743cf49990e04bfc234
Alvin Vogelzang <alvinvogelzang@gmail.com>
2024-01-24 23:42:18 +0100

    setup

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Kwiwi.io</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Kwiwi.io" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          A company by A. Vogelzang Holding B.V.
         </p>
       </main>
 
